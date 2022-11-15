891cbf4239d769c14929153a880360ef831c1e2a
Abraham_Kol <Abrahamkolawole87@gmail.com>
2022-11-15 11:14:09 +0100

    Label value generator setup

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
+import { Toaster } from 'react-hot-toast';
+
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
+}
